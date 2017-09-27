1cadc45b5f0177af8971daf98c2bce75386a91c7
shashank shashikant parab <sp2339@afsconnect2.njit.edu>
2017-09-27 17:59:39 -0400

    Adding repeat function

--- index.php
+++ index.php
@@ -29,12 +29,12 @@
   class stringFunctions {
      static public function printThis($text) {
         print($text);
-        
-        if(i%2==0){
-        {
-          $text = "<hr><br>";
-	        print($text);
         }
+
+	static public function printThisline() {
+	       $text='<hr><br>';
+	       print($text);
+	 }
 	
      }
 
@@ -44,31 +44,43 @@
 	stringFunctions::printThis($text);
 	$len=strlen($text1);
 	stringFunctions::printThis($len);
+	stringFunctions::printThisline($len);
+	
 
 	 $text='<h1>To print number of words in string<h1><br>';
 	 stringFunctions::printThis($text);
 	 $len=str_word_count($text1);
 	 stringFunctions::printThis($len);
+	 stringFunctions::printThisline($len);
+
 
 	  $text='<h1>To reverse  string<h1><br>';
 	  stringFunctions::printThis($text);
 	  $len=strrev($text1);
 	  stringFunctions::printThis($len);
+	  stringFunctions::printThisline($len);
+
 
 	  $text='<h1>To convert string to uppercase<h1><br>';
 	   stringFunctions::printThis($text);
 	  $len=strtoupper($text1);
 	  stringFunctions::printThis($len);
+	  stringFunctions::printThisline($len);
+
 
 	  $text='<h1>To convert string to lowercase<h1><br>';
 	  stringFunctions::printThis($text);
 	  $len=strtolower($text1);
 	  stringFunctions::printThis($len);
+	  stringFunctions::printThisline($len);
+
 
    	  $text='<h1>To repeat string function<h1><br>';
 	  stringFunctions::printThis($text);
 	  $len=str_repeat($text1);
 	  stringFunctions::printThis($len);
+	  stringFunctions::printThisline($len);
+
 
 
 
